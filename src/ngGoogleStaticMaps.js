141d9e8d2386b7421a0f98e43d2b2a415dac5a34
LUIZ PATRIANI <duartepatriani@gmail.com>
2015-05-27 23:42:19 -0300

    fix minify

--- src/ngGoogleStaticMaps.js
+++ src/ngGoogleStaticMaps.js
@@ -1,44 +1,41 @@
-(function () {
-	'use strict';
+function GoogleStaticMaps() {
+    var directive = {
+        link: link,
+		template: '<img alt="Google Static Map">',
+		replace: true,
+		restrict: 'E',
+    };
+    function link(scope, element, attrs, ngModel) {
+		var el = element[0];
 
-	angular.module('ngGoogleStaticMaps', []).directive('staticMap', function ($parse) {
-		return {
-			template: '<img alt="Google Static Map">',
-			replace: true,
-			restrict: 'E',
-			link: function postLink(scope, element, attrs, ctrl) {
+		if (!attrs.mapWidth) {
+			throw new Error('The `mapWidth` is required.');
+		}
 
-				var el = element[0];
-				var markers = $parse(attrs.markers)(scope);
+		if (!attrs.mapHeight) {
+			throw new Error('The `mapHeight` is required.');
+		}
 
-				if (!attrs.mapWidth) {
-					throw new Error('The `mapWidth` is required.');
-				}
+		if (!attrs.address) {
+			throw new Error('The `address` is required.');
+		}
 
-				if (!attrs.mapHeight) {
-					throw new Error('The `mapHeight` is required.');
-				}
+		var zoomParam = "&zoom=";
+		if (!attrs.zoom) {
+			zoomParam += "14"    
+		} else {
+			zoomParam += attrs.zoom
+		}
 
-				if (!attrs.address) {
-					throw new Error('The `address` is required.');
-				}
+		var urlBase = '//maps.googleapis.com/maps/api/staticmap?center=';
 
-				var zoomParam = "&zoom=";
-				if (!attrs.zoom) {
-					zoomParam += "14"    
-				} else {
-					zoomParam += attrs.zoom
-				}
+		var sizeParam = "&size="+ attrs.mapWidth +"x" + attrs.mapHeight;
+		var endEncoding = encodeURIComponent(attrs.address);
+		var markerParam = "&markers=color:red|" + endEncoding;
 
-				var urlBase = '//maps.googleapis.com/maps/api/staticmap?center=';
-
-				var sizeParam = "&size="+ attrs.mapWidth +"x" + attrs.mapHeight;
-				var endEncoding = encodeURIComponent(attrs.address);
-				var markerParam = "&markers=color:red|" + endEncoding;
-
-				el.alt = attrs.address;
-				el.src = urlBase + endEncoding + sizeParam + zoomParam + markerParam;
-    		}
-  		};
-	});
-}());
+		el.alt = attrs.address;
+		el.src = urlBase + endEncoding + sizeParam + zoomParam + markerParam;
+    }
+    return directive;
+}
+angular.module('ngGoogleStaticMaps', []).directive('staticMap',GoogleStaticMaps);
