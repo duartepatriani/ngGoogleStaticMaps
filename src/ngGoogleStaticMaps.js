288d2015e5bfa10cbedd9b296aca92c74d5ddab6
Vinicius Salvati Melquiades <vinicius.melquiades@outlook.com>
2015-07-22 17:13:17 -0300

    Configuring the protocol to use
    So that I could use the protocol I wanted, in case of running in a Cordova APP, or simply forcing HTTPS regardless of my connection I created a Provider (staticMapProvider) and added a single method (forceProtocol) that would force the specified protocol to be used.

--- src/ngGoogleStaticMaps.js
+++ src/ngGoogleStaticMaps.js
@@ -1,11 +1,35 @@
-function GoogleStaticMaps() {
-    var directive = {
-        link: link,
+function StaticMapProvider() {
+
+	var _protocol;
+
+	this.forceProtocol = function (protocol) {
+		if (angular.isString(protocol)) {
+
+			_protocol = protocol ? (protocol.replace(/:$/, '') + ':') : protocol;
+			return this;
+		}
+
+		return _protocol;
+	}
+
+	this.$get = [function () {
+		return {
+			getProtocol: function () {
+				return _protocol || '';
+			}
+		};
+	}];
+}
+
+GoogleStaticMaps.$inject = ['staticMap'];
+function GoogleStaticMaps(staticMap) {
+	var directive = {
+		link: link,
 		template: '<img alt="Google Static Map">',
 		replace: true,
 		restrict: 'E',
-    };
-    function link(scope, element, attrs, ngModel) {
+	};
+	function link(scope, element, attrs) {
 		var el = element[0];
 
 		if (!attrs.mapWidth) {
@@ -22,20 +46,20 @@ function GoogleStaticMaps() {
 
 		var zoomParam = "&zoom=";
 		if (!attrs.zoom) {
-			zoomParam += "14"    
+			zoomParam += "14"
 		} else {
 			zoomParam += attrs.zoom
 		}
 
-		var urlBase = '//maps.googleapis.com/maps/api/staticmap?center=';
+		var urlBase = staticMap.getProtocol() + '//maps.googleapis.com/maps/api/staticmap?center=';
 
-		var sizeParam = "&size="+ attrs.mapWidth +"x" + attrs.mapHeight;
+		var sizeParam = "&size=" + attrs.mapWidth + "x" + attrs.mapHeight;
 		var endEncoding = encodeURIComponent(attrs.address);
 		var markerParam = "&markers=color:red|" + endEncoding;
 
 		el.alt = attrs.address;
 		el.src = urlBase + endEncoding + sizeParam + zoomParam + markerParam;
-    }
-    return directive;
+	}
+	return directive;
 }
-angular.module('ngGoogleStaticMaps', []).directive('staticMap',GoogleStaticMaps);
+angular.module('ngGoogleStaticMaps', []).provider('staticMap', StaticMapProvider).directive('staticMap', GoogleStaticMaps);
